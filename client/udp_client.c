5b497d71fe463154207b0740fd8a3205cd59b25c
o-lee <olee@uccs.edu>
2017-04-30 19:08:26 -0600

    updated with stop-and-wait

--- client/udp_client.c
+++ client/udp_client.c
@@ -11,70 +11,120 @@
 #define SERVER_UDP_PORT         2476
 #define MAXLEN                  4096
 #define DEFLEN                  64 
+
+typedef struct packet
+{
+    char file_buffer[MAXLEN];
+}Packet;
+
+typedef struct frame
+{
+    int type;     // ack = 0; seq = 1; last message for request = 2
+    int seq_no;         //Sending sequence #
+    int ack;                // Ack #;
+    int retrans_no;     // Retransmission count
+    Packet packet;
+}Frame;
+
 long delay(struct timeval t1, struct timeval t2)
 {
-	long d;
-	d = (t2.tv_sec -t1.tv_sec) * 1000;
-	d += ((t2.tv_usec -t1.tv_usec + 500) / 1000);
-	return(d);
+    long d;
+    d = (t2.tv_sec -t1.tv_sec) * 1000;
+    d += ((t2.tv_usec -t1.tv_usec + 500) / 1000);
+    return(d);
 }
 int main(int argc, char **argv)
 {
-	int     data_size = DEFLEN, port = SERVER_UDP_PORT;
-	int     i, j, sd, server_len;
-	char    *pname, *host, rbuf[MAXLEN], sbuf[MAXLEN];
-	struct  hostent         *hp;
-	struct  sockaddr_in     server;
-	struct  timeval         start, end;
-	unsigned long address;	
-	if (argc != 5) {
-		printf("usage: ./UDP_client server file packet_loss protocol_type\n");
+    int     data_size = DEFLEN, port = SERVER_UDP_PORT;
+    int     i, j, sd; 
+    socklen_t server_len;
+    char    *pname, *host, rbuf[MAXLEN], sbuf[MAXLEN];
+    struct  hostent         *hp;
+    struct  sockaddr_in     server;
+    struct  timeval         start, end;
+    unsigned long address;	
+    if (argc != 5) {
+        printf("usage: ./UDP_client server file packet_loss protocol_type\n");
         fprintf(stderr, "Protocol Types:\n");
         fprintf(stderr, "Default: None\n");
         fprintf(stderr, "2: Go Back N\n");
-		exit(1);
-	}
-	if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		fprintf(stderr, "Can't create a socket\n");
-		exit(1);
-	}
-	bzero((char *)&server, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	if ((hp = gethostbyname(argv[1])) == NULL) {
-		fprintf(stderr, "Can't get server's IP address\n");
-		exit(1);
-	}
-	bcopy(hp->h_addr, (char *) &server.sin_addr, hp->h_length);
-	if (data_size > MAXLEN) {
-		fprintf(stderr, "Data is too big\n");
-		exit(1);
-	}
-	gettimeofday(&start, NULL); /* start delay measurement */
-	server_len = sizeof(server);
-	char *file = argv[2];
-	if (sendto(sd, file, data_size, 0, (struct sockaddr *)&server, server_len) == -1) {
-		fprintf(stderr, "sendto error\n");
-		exit(1);
-	}
-	
-	FILE *fp = fopen(file,"wb");
-	char ack[1] = {1};
-	char noack[1] = {0};
-	struct timeval tv;
-	tv.tv_sec = 7;
-	tv.tv_usec = 0;
+        exit(1);
+    }
+    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        fprintf(stderr, "Can't create a socket\n");
+        exit(1);
+    }
+    bzero((char *)&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    if ((hp = gethostbyname(argv[1])) == NULL) {
+        fprintf(stderr, "Can't get server's IP address\n");
+        exit(1);
+    }
 
-	setsockopt(sd,SOL_SOCKET,SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
+    bcopy(hp->h_addr, (char *) &server.sin_addr, hp->h_length);
+        if (data_size > MAXLEN) {
+        fprintf(stderr, "Data is too big\n");
+        exit(1);
+    }
+    gettimeofday(&start, NULL); /* start delay measurement */
+    server_len = sizeof(server);
+    char *file = argv[2];
+    if (sendto(sd, file, data_size, 0, (struct sockaddr *)&server, server_len) == -1) {
+        fprintf(stderr, "sendto error\n");
+        exit(1);
+    }
 
-	int drop = atoi(argv[3]);
-	int count = 0;
-	if (atoi(argv[4]) == 2) {
-	// GO BACK N
+    FILE *fp;
+    fp = fopen(file,"wb");
+    char ack[1] = {1};
+    char noack[1] = {0};
+    struct timeval tv;
+    tv.tv_sec = 7;
+    tv.tv_usec = 0;
+
+    setsockopt(sd,SOL_SOCKET,SO_RCVTIMEO, (const char*)&tv,sizeof(struct timeval));
+
+    int drop = atoi(argv[3]);
+    int count = 0;
+    if ( atoi(argv[4]) == 1 )
+    {
+        // Stop-and-wait control flow --Oi Lee
+        // Utilizes Frame and Packet Struct
+        printf("%s\n", "Utilizing Stop-and-Wait protocol");
+        Frame frame_send;
+        Frame frame_recv;
+        int frame_id = 0;
+        int recv_size;
+
+        while(1) {
+            recv_size = recvfrom(sd, &frame_recv, sizeof(Frame), 0 , (struct sockaddr *)&server, &server_len);
+
+            if (frame_recv.seq_no == frame_id)
+            {
+                frame_send.seq_no = 0;
+                frame_send.type = 0;
+                frame_send.ack = frame_recv.seq_no;
+                sendto(sd, &frame_send, sizeof(frame_send), 0, (struct sockaddr *)&server, server_len);
+
+                fwrite(frame_recv.packet.file_buffer, 64, 1, fp);
+                frame_id++;
+            }
+        }
+
+        fclose(fp);
+        gettimeofday(&end, NULL); /* end delay measurement */
+        close(sd);
+        printf("#TRANSFER COMPLETE total packets :%d\n", frame_id);
+
+        return(0);
+    }
+    else if (atoi(argv[4]) == 2) {
+        // GO BACK N
         printf("%s\n", "Starting Go Back N");
-	    int rxId = 0;
-	    int expectedId = 0;
-	    int justSkipped = 1;
+        int rxId = 0;
+        int expectedId = 0;
+        int justSkipped = 1;
         while (1) {
             recvfrom(sd, &rxId, sizeof(rxId), 0 , (struct sockaddr *)&server, &server_len);
             int parseId = ntohl(rxId);
@@ -93,14 +143,14 @@ int main(int argc, char **argv)
                 justSkipped = 1;
                 sendto(sd, &expectedIdParse, sizeof(expectedIdParse), 0, (struct sockaddr *)&server, server_len);
             }
-
         }
         fclose(fp);
         gettimeofday(&end, NULL); /* end delay measurement */
         close(sd);
         printf("#TRANSFER COMPLETE total packets :%d\n", ntohl(rxId));
         return(0);
-	} else {
+    } 
+    else {
         while (1) {
             int n = recvfrom(sd, rbuf, 64, 0 , (struct sockaddr *)&server, &server_len);
             if (n <= 0) break;
@@ -112,11 +162,10 @@ int main(int argc, char **argv)
             }
             fwrite(rbuf,64,1,fp);
             count = count + 1;
-
         }
         fclose(fp);
         gettimeofday(&end, NULL); /* end delay measurement */
         close(sd);
         return(0);
-	}
+    }
 }
--- server/udp_server.c
+++ server/udp_server.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -10,64 +11,122 @@
 #define SERVER_UDP_PORT 2476 
 #define MAXLEN 4096 
 
+typedef struct packet
+{
+    char file_buffer[MAXLEN];
+}Packet;
+
+typedef struct frame
+{
+    int type;     // ack = 0; seq = 1; last message for request = 2
+    int seq_no;         //Sending sequence #
+    int ack;                // Ack #;
+    int retrans_no;     // Retransmission count
+    Packet packet;
+}Frame;
+
 int main(int argc, char **argv)
 {
-	int sd, client_len, port, n;
-	char buf[MAXLEN];
-	struct sockaddr_in 	server, client;
-	switch(argc) {
-		case 2:
-			port = SERVER_UDP_PORT;
-			break;
-		default:
-			fprintf(stderr, "Usage: %s [protocol_type]\n", argv[0]);
-			fprintf(stderr, "Protocol Types:\n");
-			fprintf(stderr, "Default: None\n");
-			fprintf(stderr, "2: Go Back N\n");
-			exit(1);
-	}
-	/* Create a datagram socket */
-	if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		fprintf(stderr, "Can't create a socket\n");
-		exit(1);
-	}
-	/* Bind an address to the socket */
-	bzero((char *)&server, sizeof(server));
-	server.sin_family = AF_INET;
-	server.sin_port = htons(port);
-	server.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
-		fprintf(stderr, "Can't bind name to socket\n");
-		exit(1);
-	}
-	FILE *fp;
-	size_t file_size;
-	char file_buffer[2000];
-	int bytes;
-	char ack[1];
-	int pkt_count=0;
-	if (atoi(argv[1]) != 2) {
-        while (1) {
-	    printf("%s\n", "1");
+    int sd, port, n;
+    socklen_t client_len;
+    char buf[MAXLEN];
+    struct sockaddr_in 	server, client;
+    switch(argc) {
+        case 2:
+            port = SERVER_UDP_PORT;
+            break;
+        default:
+            fprintf(stderr, "Usage: %s [protocol_type]\n", argv[0]);
+            fprintf(stderr, "Protocol Types:\n");
+            fprintf(stderr, "Default: None\n");
+            fprintf(stderr, "2: Go Back N\n");
+            exit(1);
+    }
+    /* Create a datagram socket */
+    if ((sd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        fprintf(stderr, "Can't create a socket\n");
+        exit(1);
+    }
+    /* Bind an address to the socket */
+    bzero((char *)&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (bind(sd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+        fprintf(stderr, "Can't bind name to socket\n");
+        exit(1);
+    }
+
+    FILE *fp;
+    size_t file_size;
+    char file_buffer[2000];
+    char ack[1];
+    int pkt_count=0;
+
+    if (atoi(argv[1]) == 1) {   
+        // Stop-and-wait control flow --Oi Lee
+        // Utilizes Frame and Packet Struct
+        printf("%s\n", "Utilizing Stop-and-Wait protocol");
+        Frame frame_send;
+        Frame frame_recv;
+        int frame_id = 0;
+        int recv_size;
+        int ack = 1;
+        fp = fopen(buf,"rb");
+
+        while(1) {
             client_len = sizeof(client);
             if ((recvfrom(sd, buf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {
                 fprintf(stderr, "Can't receive datagram\n");
                 exit(1);
             }
             fp = fopen(buf,"rb");
-            while (!feof(fp)){
-                fread(file_buffer,64,1,fp);
-                printf("sending pkt: %d\n",pkt_count);
-                sendto(sd, file_buffer, 64, 0, (struct sockaddr *)&client, client_len);
-                recvfrom(sd,ack,1,0,(struct sockaddr *)&client, &client_len);
-                printf("%d ack: %d\n\n",pkt_count,ack[0]);
-                pkt_count = pkt_count+1;
+
+            // Check if file opened
+            if(fp == NULL) {
+                printf("Error in file opening\n");
+                exit(1);
+            }
+
+            while( !feof(fp) ) {
+                if (ack == 1) {
+                    // create frame
+                    frame_send.type = 1;
+                    frame_send.seq_no = frame_id;
+                    frame_send.ack = 0;
+                    frame_send.retrans_no = 0;
+                    fread(frame_send.packet.file_buffer,64,1,fp);
+
+                    printf("[+] Sending Frame: %d\n", frame_send.seq_no);
+                    sendto(sd, &frame_send, sizeof(Frame), 0, (struct sockaddr *)&client, client_len);
+                    frame_id++;
+                }
+                else
+                {
+                    frame_send.retrans_no++;
+                    printf("[+] Resending Frame: %d\n", frame_send.seq_no);
+                    sendto(sd, &frame_send, sizeof(Frame), 0, (struct sockaddr *)&client, client_len);
+                }
+
+                recv_size =  recvfrom(sd, &frame_recv, sizeof(frame_recv), 0, (struct sockaddr *)&client, &client_len);
+
+                if ( recv_size > 0 && frame_recv.type == 1 && frame_recv.ack == frame_send.seq_no)
+                {
+                    printf("[+] Ack received. Ack #: %d\n", frame_send.ack);
+                    ack = 1;
+                }
+                else 
+                {
+                    ack = 0;
+                }
+
             }
         }
-	} else {
-	// GO BACK N
+    } 
+    else if (atoi(argv[1]) == 2) {
+        // GO BACK N
         printf("%s\n", "Starting Go Back N");
-	    struct timespec start, end;
+        struct timespec start, end;
         while (1) {
             client_len = sizeof(client);
             if ((recvfrom(sd, buf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {
@@ -109,8 +168,28 @@ int main(int argc, char **argv)
                 pkt_count = pkt_count+1;
             }
         }
-	}
-	fclose(fp);
-	close(sd);
-	return(0);
+    }
+    else {
+        while (1) {
+            printf("%s\n", "1");
+            client_len = sizeof(client);
+            if ((recvfrom(sd, buf, MAXLEN, 0, (struct sockaddr *)&client, &client_len)) < 0) {
+                fprintf(stderr, "Can't receive datagram\n");
+                exit(1);
+            }
+            fp = fopen(buf,"rb");
+            while (!feof(fp)){
+                fread(file_buffer,64,1,fp);
+                printf("sending pkt: %d\n",pkt_count);
+                sendto(sd, file_buffer, 64, 0, (struct sockaddr *)&client, client_len);
+                recvfrom(sd,ack,1,0,(struct sockaddr *)&client, &client_len);
+                printf("%d ack: %d\n\n",pkt_count,ack[0]);
+                pkt_count = pkt_count+1;
+            }
+        }
+    }
+
+    fclose(fp);
+    close(sd);
+    return(0);
 }
